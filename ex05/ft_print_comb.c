9109b3af70dd5da1b65ba5c11adbcecc22cab3ec
Youngjun Lee <youngjle@c8r6s9.42seoul.kr>
2020-10-27 01:12:42 +0900

    mod ex05

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,7 +6,7 @@
 /*   By: youngjle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 21:35:54 by youngjle          #+#    #+#             */
-/*   Updated: 2020/10/26 21:17:29 by youngjle         ###   ########.fr       */
+/*   Updated: 2020/10/27 01:07:57 by youngjle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,34 +17,41 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_comb(void)
+void	ft_print_comb_calc(char a, char b, char c)
 {
-	char number[3];
-
-	number[0] = '0';
-	number[1] = '1';
-	number[2] = '2';
-	while (number[0] <= '7')
+	while (a <= '7')
 	{
-		while (number[1] <= '8')
+		while (b <= '8')
 		{
-			while (number[2] <= '9')
+			while (c <= '9')
 			{
-				ft_putchar(number[0]);
-				ft_putchar(number[1]);
-				ft_putchar(number[2]);
+				ft_putchar(a);
+				ft_putchar(b);
+				ft_putchar(c);
 				if (a != '7' || b != '8')
 				{
 					ft_putchar(',');
 					ft_putchar(' ');
 				}
-				number[2]++;
+				c++;
 			}
-			number[1]++;
-			number[2] = number[1] + 1;
+			b++;
+			c = b + 1;
 		}
-		number[0]++;
-		number[1] = number[0] + 1;
-		number[2] = number[1] + 1;
+		a++;
+		b = a + 1;
+		c = b + 1;
 	}
+}
+
+void	ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+
+	a = '0';
+	b = '1';
+	c = '2';
+	ft_print_comb_calc(a, b, c);
 }
